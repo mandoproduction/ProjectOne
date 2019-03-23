1c7bdd415950865d5a08c85084ee284476997e21
Craig <craigdwyer73@gmail.com>
2019-03-23 12:39:55 -0400

    firebase

---
+++ javascript/app.js
@@ -0,0 +1,13 @@
+//(document).ready(function(){}
+
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAtmUN_ybY7kbbMc_2rdzyvruwpIaIX4Bs",
+    authDomain: "project1-2489b.firebaseapp.com",
+    databaseURL: "https://project1-2489b.firebaseio.com",
+    projectId: "project1-2489b",
+    storageBucket: "project1-2489b.appspot.com",
+    messagingSenderId: "798150942223"
+  };
