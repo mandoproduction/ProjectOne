066bc118c91b6da1ea378691aeef2f91cc4801d2
Bonesaw <dalantinow@gmail.com>
2019-03-26 19:08:42 -0400

    changes

--- urbandictionary.js
+++ urbandictionary.js
@@ -6,25 +6,28 @@ var config = {
     storageBucket: "project1-2489b.appspot.com",
     messagingSenderId: "798150942223"
 };
-
+var title = "";
 firebase.initializeApp(config);
 var database = firebase.database();
 
-var title = ""
+
 var urbanURL = "https://api.urbandictionary.com/v0/define?term={" + title + "}";
-var websterURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + title + "?key=9b48b980-097f-4626-9bc3-c269d87eb657"
+var websterURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + title + "?key=9b48b980-097f-4626-9bc3-c269d87eb657";
 
 $("#search-term").on("click", function (event) {
     event.preventDefault();
 
-    title = $("search-query").val().trim();
+    title = $("#search-query").val().trim();
+
+    var urbanURL = "https://api.urbandictionary.com/v0/define?term={" + title + "}";
+    var websterURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + title + "?key=9b48b980-097f-4626-9bc3-c269d87eb657";
 
     $.ajax({
         url: urbanURL,
-        method: "GET"
+        method: "GET",
     }).then(function (responseUrban) {
         var urbanDef = responseUrban.list[0].definition.replace(/\[|\]|\(|\)/g, "")
-        console.log(urbanDef)
+       console.log(urbanDef)
     });
     $.ajax({
         url: websterURL,
@@ -39,13 +42,13 @@ $("#search-term").on("click", function (event) {
             title: title
         };
 
-    database.ref().push(newTerm);
+        database.ref().push(newTerm);
 
-    $("#search-query").val("");
+        $("#search-query").val("");
     });
 });
 
-database.ref().on("child_added", function(snapshot){
+database.ref().on("child_added", function (snapshot) {
     var title = snapshot.val().title;
     var urbanDef = snapshot.val().urbanDef;
     var websterDef = snapshot.val().websterDef;
@@ -54,4 +57,4 @@ database.ref().on("child_added", function(snapshot){
     $("<body>").append(urbanDef);
     $("<body>").append(websterDef);
 
-})
+});
